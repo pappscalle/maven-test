7364adfda940a72725cd1b631644b606f72e43d8
Calle <carl.frenn@gmail.com>
2020-10-14 08:41:40 +0200

    fixed typo

--- src/main/java/se/tastebin/Main.java
+++ src/main/java/se/tastebin/Main.java
@@ -3,8 +3,7 @@ package se.tastebin;
 public class Main {
     
     public static void main(String[] args) {
-        HelloWorld hw = new HelloWorld("Hello", "World");
-        System.out.println(hw.value());     
+        System.out.println(new HelloWorld("Hello", "World").value());     
     }
     
 }
--- src/test/java/se/tastebin/HelloWorldShould.java
+++ src/test/java/se/tastebin/HelloWorldShould.java
@@ -1,13 +1,12 @@
 package se.tastebin;
 
-import se.tastebin.HelloWorld;
 import static org.testng.Assert.*;
 import org.testng.annotations.Test;
 
 public class HelloWorldShould {
     
     @Test
-    public void returnHelloWorl() {
+    public void returnHelloWorld() {
         assertEquals(new HelloWorld("Hello", "World").value(), "Hello World!");
     }
     
